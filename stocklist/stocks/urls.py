6c4de01f5456c5367c60c9d4f83f249f9ba59189
NoahColaco <noahcolaco@gmail.com>
2021-05-08 16:32:23 -0400

    init django

--- stocklist/stocks/urls.py
+++ stocklist/stocks/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('stock', views.returnStock, name='returnStock')
 ]
--- stocklist/stocks/views.py
+++ stocklist/stocks/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+import requests
 
+
+def returnStock(request):  
+
+    url = "https://finnhub.io/api/v1/search?q=tesla"
+
+    response = requests.request("GET", url, params=headers)
+
+    return HttpResponse(response)
 
 def index(request):
     return HttpResponse("Hi Sinan")
